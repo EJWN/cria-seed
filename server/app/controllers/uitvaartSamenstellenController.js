3784a4b4d5f64a23eb8bd3e150d69ae134c47fa3
jurbraam94 <jur_braam@hotmail.com>
2015-05-26 11:58:17 +0200

    format

--- server/app/controllers/uitvaartSamenstellenController.js
+++ server/app/controllers/uitvaartSamenstellenController.js
@@ -1,20 +1,20 @@
-﻿    "use strict";
+﻿"use strict";
 
-    var mongoose = require('mongoose'),
-        uitvaartSamenstellen = mongoose.model('uitvaartSamenstellen');
+var mongoose = require('mongoose'),
+    uitvaartSamenstellen = mongoose.model('uitvaartSamenstellen');
 
-    exports.detail = function (req, res) {
-        var conditions, fields;
+exports.detail = function (req, res) {
+    var conditions, fields;
 
-        conditions = { _id: req.params._id };
-        fields = {};
+    conditions = { _id: req.params._id };
+    fields = {};
 
-        uitvaartSamenstellen.findOne(conditions, fields).exec(function (err, doc) {
-            var retObj = {
-                meta: { "action": "detail", 'timestamp': new Date(), filename: __filename },
-                doc: doc,
-                err: err
-            };
-            return res.send(retObj);
-        });
-    };
+    uitvaartSamenstellen.findOne(conditions, fields).exec(function (err, doc) {
+        var retObj = {
+            meta: { "action": "detail", 'timestamp': new Date(), filename: __filename },
+            doc: doc,
+            err: err
+        };
+        return res.send(retObj);
+    });
+};
